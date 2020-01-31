d07c8a91182fe8f01c30fdb35c8bcc1c5c0b2b2c
Karl Hulme <karlhulme@hotmail.com>
2020-01-31 10:34:31 +0000

    --fix Fixed export statement for createJsonSchemaForDocTypeInstance.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
   createJsonotron: require('./src/engine/createJsonotron'),
-  createJsonSchemaForDocTypeInstance: require('.src/docTypes/createJsonSchemaForDocTypeInstance'),
+  createJsonSchemaForDocTypeInstance: require('./src/docTypes/createJsonSchemaForDocTypeInstance'),
   errors: require('./src/errors')
 }
