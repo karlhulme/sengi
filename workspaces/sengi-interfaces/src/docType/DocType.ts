c40672ca7d5c140eab2adb94537702374ef4447c
Karl Hulme <karlhulme@hotmail.com>
2021-05-09 12:04:32 +0100

    --break Updating interfaces to remove dependency on jsonotron.

--- workspaces/sengi-interfaces/src/docType/DocType.ts
+++ workspaces/sengi-interfaces/src/docType/DocType.ts
@@ -1,22 +1,51 @@
-import { DocTypeField } from './DocTypeField'
-import { DocTypeCalculatedField } from './DocTypeCalculatedField'
-import { DocTypeFilter } from './DocTypeFilter'
-import { DocTypeAggregate } from './DocTypeAggregate'
-import { DocTypeConstructor } from './DocTypeConstructor'
-import { DocTypeOperation } from './DocTypeOperation'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { DocTypePolicy } from './DocTypePolicy'
 import { DocStoreOptions } from '../docStore'
-import { Doc, DocExample } from '../doc'
 
-export interface DocType {
+export interface DocTypeConstructor<Doc, Parameters> {
+  summary: string
+  deprecation?: string
+  parametersJsonSchema: string
+  implementation: (parameters: Parameters) => Doc
+}
+
+export interface DocTypeFilter<Filter, Parameters> {
+  summary: string
+  deprecation?: string
+  parametersJsonSchema: string
+  implementation: (parameters: Parameters) => Filter
+}
+
+export interface DocTypeOperation<Doc, Parameters> {
+  summary: string
+  deprecation?: string
+  parametersJsonSchema: string
+  implementation: (doc: Doc, parameters: Parameters) => void
+}
+
+export interface DocTypeAggregate<Response, Parameters> {
+  summary: string
+  deprecation?: string
+  responseJsonSchema: string
+  parametersJsonSchema: string
+  implementation: (parameters: Parameters) => Response
+}
+
+export interface DocType<Doc, Filter> {
   name: string
   pluralName: string
   title: string
   pluralTitle: string
   summary: string
-  documentation: string
 
-  fields: Record<string, DocTypeField>
+  jsonSchema: string
+
+  ctor?: DocTypeConstructor<Doc, any>
+
+  filters?: Record<string, DocTypeFilter<Filter, any>>
+
+  operations?: Record<string, DocTypeOperation<Doc, any>>
 
   /**
    * A function that can perform cleanup adjustments on a document, such as removing 
@@ -30,13 +59,74 @@ export interface DocType {
    */
   validate?: (doc: Doc) => void
 
-  examples: DocExample[]
-  calculatedFields: Record<string, DocTypeCalculatedField>
-  filters: Record<string, DocTypeFilter>
-  aggregates: Record<string, DocTypeAggregate>
-  ctor: DocTypeConstructor
-  operations: Record<string, DocTypeOperation>
+  policy?: DocTypePolicy
 
-  policy: DocTypePolicy
-  docStoreOptions: DocStoreOptions
+  docStoreOptions?: DocStoreOptions
 }
+
+export interface FieldShape {
+  a: string
+  b: number
+  c: boolean
+} 
+
+export interface ExampleParameters {
+  d: string
+  e: number
+}
+
+export interface ExampleFilter {
+  awsId: string
+  awsSort: string
+}
+
+export interface ExampleAggegate {
+  getCount: boolean
+}
+
+const testDocType: DocType<FieldShape, ExampleFilter> = {
+  name: 'test',
+  pluralName: 'tests',
+  title: 'Test',
+  pluralTitle: 'Tests',
+  summary: 'Use the type as follows...',
+  jsonSchema: 'type=object,properties,etc,etc',
+  ctor: {
+    summary: 'Create a new instance of the type.',
+    parametersJsonSchema: '....',
+    implementation: (parameters: ExampleParameters) => ({
+      a: parameters.d,
+      b: 123,
+      c: true
+    })
+  },
+  filters: {
+    byExample: {
+      summary: 'A filter',
+      parametersJsonSchema: '...',
+      implementation: (parameters: ExampleParameters) => ({
+        awsId: parameters.d,
+        awsSort: 'up'
+      })
+    }
+  },
+  operations: {
+    addSignature: {
+      summary: 'Add Signature',
+      parametersJsonSchema: '...',
+      implementation: (doc, parameters: ExampleParameters) => {
+        doc.a = parameters.d
+      }
+    }
+  },
+  preSave: shape => {
+    shape.a = 'something else'
+  },
+  validate: shape => {
+    if (shape.a === 'whatever') {
+      throw new Error()
+    }
+  }
+}
+
+console.log(testDocType)
